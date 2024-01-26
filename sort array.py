3ee2e235089f12a30e0b2b3f56699b18647f7cd6
onedeadgo <134501679+onedeadgo@users.noreply.github.com>
2024-01-26 15:25:03 +0530

    Add files via upload

---
+++ sort array.py
@@ -0,0 +1,19 @@
+# Example array
+my_array = [5, 2, 8, 1, 3]
+
+# Sorting the array
+sorted_array = sorted(my_array)
+
+# Printing the sorted array
+print("Sorted Array:", sorted_array)
+
+
+# Example array
+my_array = [5, 2, 8, 1, 3]
+
+# Sorting the array in-place using sort()
+my_array.sort()
+
+# Printing the sorted array
+print("Sorted Array:", my_array)
+
