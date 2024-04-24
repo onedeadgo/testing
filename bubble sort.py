edefd2275cdb83a04672fe0669a71564536e8b30
SAURABH PATEL <134501679+onedeadgo@users.noreply.github.com>
2024-04-24 11:08:51 +0530

    Update bubble sort.py
    updated

--- bubble sort.py
+++ bubble sort.py
@@ -1,11 +1,44 @@
-def bubble_sort(arr):
-    n = len(arr)
-    for i in range(n):
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+def bubble_sort(data):
+  """Sorts a list of data in ascending order using bubble sort algorithm.
 
-# Example usage:
-my_list = [64, 34, 25, 12, 22, 11, 90]
-bubble_sort(my_list)
-print("Sorted array:", my_list)
+  Args:
+      data: A list of sortable elements.
+
+  Returns:
+      The sorted list.
+  """
+  # Get the length of the list
+  n = len(data)
+  
+  # Outer loop to handle number of passes
+  for i in range(n-1):
+    swapped = False
+    
+    # Inner loop to compare adjacent elements
+    for j in range(0, n-i-1):
+      if data[j] > data[j + 1]:
+        # Swap elements if they are in the wrong order
+        data[j], data[j + 1] = data[j + 1], data[j]
+        swapped = True
+    
+    # If we haven't made any swaps in this pass, the list is already sorted
+    if not swapped:
+      break
+  
+  return data
+
+# Get user input for the list
+data = []
+num_elements = int(input("Enter the number of elements: "))
+for i in range(num_elements):
+  element = int(input("Enter element {}: ".format(i+1)))
+  data.append(element)
+
+# Print the unsorted list
+print("Unsorted list:", data)
+
+# Sort the list using bubble sort
+sorted_data = bubble_sort(data)
+
+# Print the sorted list
+print("Sorted list:", sorted_data)
